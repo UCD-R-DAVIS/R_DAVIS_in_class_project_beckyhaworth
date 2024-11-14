fc7464d6ee8679f3d0d9f1a31d68a218851c23eb
Becky Haworth <rshaworth@ucdavis.edu>
2024-11-14 13:31:27 -0800

    in class updates

--- scripts/week_7_homework.R
+++ scripts/week_7_homework.R
@@ -8,7 +8,7 @@ select_gapminder <- gapminder %>%
   filter(year == 2002 | year == 2007) %>% 
   filter(!continent == "Oceania") %>% 
   pivot_wider(names_from = "year", values_from = "pop") %>% 
-  mutate(popchange = (`2007` - `2002`)) %>% 
+  mutate(popchange = (`2007` - `2002`)) %>%   
   arrange(continent)
 
 library("RColorBrewer")
@@ -21,7 +21,7 @@ ggplot(data = select_gapminder, mapping = aes(x = reorder(country, popchange), y
   theme(axis.text.x = element_text(angle=45, hjust=1)) +
   facet_wrap(~ continent, scale = 'free') +
   ylab("Change in Population Between 2002 and 2007")+
-  xlab("County")
+  xlab("County") # + coord_flip (flips data on x and y axis)
   
   
 
