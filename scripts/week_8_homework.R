52bb061c8b3d9376ce16146869d1279c03cf8af1
Becky Haworth <rshaworth@ucdavis.edu>
2024-11-21 13:45:43 -0800

    in class updates

--- scripts/week_8_homework.R
+++ scripts/week_8_homework.R
@@ -6,11 +6,14 @@ tz(mloa$year)
 #With the mloa data.frame, remove observations with missing values in rel_humid, temp_C_2m, and windSpeed_m_s. Generate a column called “datetime” using the year, month, day, hour24, and min columns. 
 
 mloa <- mloa %>% 
-  filter(rel_humid != -99) %>%
+  filter(rel_humid != -99) %>% #since the missing observations are negative, you could also just do != <0  or = >0 for all of these instead of the -99 values.
   filter(temp_C_2m != -999.9) %>%
   filter(windSpeed_m_s != -999.9) %>% 
-  mutate(datetime = ymd_hm(paste(year, "-", month, "-", day, " ", hour24, ":", min)))
+  mutate(datetime = ymd_hm(paste(year, "-", month, "-", day, " ", hour24, ":", min))) #probably should use paste0 to make it so there are no extra spaces, or at the end say 'sep = "-"' to tell it do put dashes between...but will need to do a separate paste function for hour and min to put : between
 
+#example - mutate(datetime = ymd_hm(paste(year, month, day, sep ="), paste(hour24, min, sep = ":")))
+
+mloa$datetime
 
 table(mloa$hour24)
   
@@ -24,7 +27,7 @@ mloa$datetimeLocal <- with_tz(mloa$datetime, tzone = "Pacific/Honolulu")
 library(dplyr)
 
 MeanTemp <- mloa %>% 
-  select(temp_C_2m, datetimeLocal) %>% 
+  select(temp_C_2m, datetimeLocal) %>% #i think selecting was unnecessary here?
   group_by(month(mloa$datetimeLocal, label = TRUE, abbr=TRUE), hour(mloa$datetimeLocal)) %>% 
   summarize(avehourlyTemp = mean(temp_C_2m))
 
@@ -43,6 +46,4 @@ ggplot(data = MeanTemp, mapping = aes(x = `month(mloa$datetimeLocal, label = TRU
   ylab("Average Temperature (degrees C)") + 
   ggtitle("Mean Hourly Monthly Temperature")
 
-
-library("RColorBrewer")
-display.brewer.all(colorblindFriendly = TRUE)
+#would be a good time to use a circular color palette because time 0 and 23 are next to each other in time but opposite ends of most color palettes. when they should be closely related.
