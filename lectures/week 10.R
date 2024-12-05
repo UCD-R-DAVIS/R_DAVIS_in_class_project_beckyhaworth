2b6ae7c1e69cf06d8c7b153f59fb8cfe3a3e5317
Becky Haworth <rshaworth@ucdavis.edu>
2024-12-05 14:49:51 -0800

    AI assisted coding

---
+++ lectures/week 10.R
@@ -0,0 +1,11 @@
+#AI assisted coding
+#Claude in Cursor, GitHub Co Pilot in Rstudio
+#Ctrl K - to open chatbox
+#example - Query the U.S. EPA SDWIS API (safe drinking water) for a list of all Community Water Systems in Alaska
+#you have to know what you're asking for, specify as much as you can
+
+#highlight everything, then hit Control K again...then ask "Now turn this into a function where i can specify a state abbreviation" 
+
+#highlight everything, control K, "Change this so that it downloads the dataset of systems and stores it in a directory that i specify"
+
+#highlight everything, control K, "Create a CLOBBER option where if CLOBBER = T, the file is overwritten, but if the file already exists and CLOBBER = F, it skips"
